d7d43650c46c4a2a98e82607b9c5a275e60fcca8
durb-afk <jethalalgada380@gmail.com>
2023-08-19 17:13:39 +1000

    test

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from db import db_userid
 
 app = Flask(__name__)
 
+@app.route("/")
+def flask_hello():
+    return "Homepage link of the website"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+# @app.route("/tweet")
+# def post_tweet(user_id):
+    
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
