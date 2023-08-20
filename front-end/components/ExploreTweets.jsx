9356c8b04838c626bcb7187dbb694377452a5b98
durb-afk <jethalalgada380@gmail.com>
2023-08-20 11:16:52 +1000

    Latest website

--- front-end/components/ExploreTweets.jsx
+++ front-end/components/ExploreTweets.jsx
@@ -32,7 +32,7 @@ export default function ExploreTweets() {
                 </ListItem>
             ))}
         </List>
-        {/* <Messages/>         */}
+        <Messages/>        
   </Box>
   );
 }
